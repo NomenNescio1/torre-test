ba073e77a3d9b48bb06a5b9b1b8248498dc60b65
NomenNescio <u1201416@unimilitar.edu.co>
2019-11-20 13:05:52 -0500

    keep testing endpoints, added user not found case, buliding remaining functionality

--- torre-test/src/components/Connections.js
+++ torre-test/src/components/Connections.js
@@ -6,22 +6,38 @@ class Connections extends React.Component {
     constructor(props){
         super(props);
         this.state  ={
-          connections: []
+          connections: [],
+          error: ''
         }
     }
+    
     getConnections = (e) =>{
+        let apiBaseEndpoint = `https://cors-anywhere.herokuapp.com/bio.torre.co/api/`;
         e.preventDefault();
-        axios.get(`https://cors-anywhere.herokuapp.com/bio.torre.co/api/people/${this.usernameRef.current.value}/connections`)
+        axios.get(`${apiBaseEndpoint}people/${this.usernameRef.current.value}/connections`)
     .then(response => {
         // handle success
-        console.log(response);
+        //console.log(response.data[0].person.publicId);        
+        let res = response.data.map((item) => {
+            return item.person.publicId;
+        });
+
         this.usernameRef.current.value = '';
         this.setState ({
-            connections: response.data
+            error: null,
+            connections: res
         })
+        let urlBio = res.map((item)=>{
+            return `${apiBaseEndpoint}bios/` + item
+        });
+        console.log(urlBio);
     })
     .catch(error => {
         // handle error
+        this.usernameRef.current.value = '';
+        this.setState({
+            error: error.isAxiosError
+        })
         console.log(error);
     })
     }
@@ -36,6 +52,9 @@ render (){
             <input type="submit" value="Go"/>
         </form>
         <h6>You can find your username here</h6>
+        <div className="connections-container">
+            <p>{this.state.error ? 'User not found. Try again?' : ''}</p>
+        </div>
     </div>
     }
 }
